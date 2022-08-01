b11a5c5883909558818e1222dccbae93917c5707
alwaysraihan <bdyoungdeveloper@gmail.com>
2022-08-01 11:28:44 +0600

    project Initial setup comleted

---
+++ src/Components/Shared/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+    <div className="flex justify-center items-center py-6 bg-blue-500">
+      <h1 className="text-center text-white text-2xl font-bold">This is Footer</h1>
+    </div>
+  
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/Pages/HomePage/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+    <div className="min-h-[85vh]">
+
+      <h1 className="text-4xl text-center font-bold pt-10 text-pink-500">It's an Simple Setup</h1>
+      <h1 className="text-4xl p-20 font-bold text-pink-500">This is home</h1>
+    </div>
+    </>
+  );
+};
+
+export default Home;
