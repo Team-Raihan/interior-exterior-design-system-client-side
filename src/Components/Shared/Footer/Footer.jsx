116dd2cb2c6e028419de0e877884f18bb0f579c8
abukawsar47 <abukawsar47ak@gmail.com>
2022-08-01 20:04:49 +0600

    search input

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -3,10 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-    <div className="flex justify-center items-center py-6 bg-blue-500">
-      <h1 className="text-center text-white text-2xl font-bold">This is Footer</h1>
-    </div>
-  
+      <div className="flex justify-center items-center py-6 bg-neutral">
+        <h1 className="text-center text-white text-2xl font-bold">
+          This is Footer
+        </h1>
+      </div>
     </>
   );
 };
--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div className="flex justify-center items-center py-6 bg-blue-500">
+      <div className="flex justify-center items-center py-6 bg-secondary">
         <h1 className="text-center text-white text-2xl font-bold">
           This is Navbar
         </h1>
