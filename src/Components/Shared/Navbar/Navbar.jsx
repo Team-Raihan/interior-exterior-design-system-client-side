0daa603feb4423d6ef37974d48a42c88d6a9e81f
abukawsar47 <abukawsar47ak@gmail.com>
2022-08-03 20:56:03 +0600

    mobile menu setup

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { signOut } from "firebase/auth";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Link as NavLink } from "react-router-dom";
+import { Link, Link as NavLink } from "react-router-dom";
 import auth from "../../../Firebase/Firebase.init";
 
 const Navbar = () => {
@@ -29,9 +29,8 @@ const Navbar = () => {
       <li>
         {user ? (
           <NavLink to="">
-            {" "}
             <div class="dropdown dropdown-end">
-              <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+              <label tabindex="0" class="btn btn-ghost  btn-circle avatar">
                 <div class="w-10 rounded-full  ring-white ring-2">
                   <img
                     src={
@@ -56,7 +55,6 @@ const Navbar = () => {
                   <NavLink to="">Dashboard</NavLink>
                 </li>
                 <li>
-                  {/* <a>Logout</a> */}
                   <NavLink to="" onClick={logout}>
                     Logout
                   </NavLink>
@@ -72,32 +70,8 @@ const Navbar = () => {
   );
   return (
     <div className="sticky top-0 bg-secondary text-white font-bold  z-40 drop-shadow-xl ">
-      <div className="container navbar mx-auto px-4 py-4">
+      <div className="container navbar md:min-h-[96px] justify-between mx-auto px-4">
         <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex="0" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex="0"
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              {menuItems}
-            </ul>
-          </div>
           <NavLink
             to="/"
             className="btn btn-ghost normal-case text-2xl font-bold"
@@ -105,15 +79,8 @@ const Navbar = () => {
             Logo
           </NavLink>
         </div>
-        <div className="navbar-end hidden lg:flex min-w-fit">
-          <ul className="menu menu-horizontal p-0">{menuItems}</ul>
-        </div>
-        <div className="navbar-end lg:hidden">
-          <label
-            tabIndex="1"
-            htmlFor="dashboard-sidebar"
-            className="btn btn-ghost lg:hidden"
-          >
+        <div className="dropdown dropdown-end">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -129,6 +96,115 @@ const Navbar = () => {
               />
             </svg>
           </label>
+          <ul
+            tabIndex="0"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-secondary rounded-box w-52"
+          >
+            <>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/products">Products</NavLink>
+              </li>
+              <li>
+                <NavLink to="/reviews">Reviews</NavLink>
+              </li>
+              <li>
+                <NavLink to="/blog">Blogs</NavLink>
+              </li>
+              <li>
+                {user ? (
+                  /*    <Link to="">
+                       <div class="dropdown dropdown-end">
+                      <ul
+                        tabindex="0"
+                        class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-secondary rounded-box w-52"
+                      ></ul>
+                    </div>
+                    <div
+                      tabIndex="0"
+                      className="collapse collapse-arrow bg-secondary text-white rounded-box"
+                    >
+                      <label
+                        tabindex="0"
+                        class="btn btn-ghost btn-circle avatar"
+                      >
+                        <div class="w-10 rounded-full  ring-white ring-2">
+                          <img
+                            src={
+                              user?.photoURL
+                                ? user?.photoURL
+                                : "https://foxdogconsultants.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png"
+                            }
+                            alt="user img"
+                          />
+                        </div>
+                      </label>
+                      <ui className="collapse-content">
+                        <li>
+                          <Link to="" class="">
+                            Profile
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="" class="">
+                          Dashboard
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="" onClick={logout}>
+                            Logout
+                          </Link>
+                        </li>
+                      </ui>
+                    </div>
+                  </Link> */
+                  <NavLink className="" to="">
+                    <div
+                      tabindex="0"
+                      class="collapse collapse-plus  bg-secondary text-white rounded-box w-full"
+                    >
+                      <label class="btn btn-ghost btn-circle avatar">
+                        <div class="w-6 rounded-full  ring-white ring-2">
+                          <img
+                            src={
+                              user?.photoURL
+                                ? user?.photoURL
+                                : "https://foxdogconsultants.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png"
+                            }
+                            alt="user img"
+                          />
+                        </div>
+                      </label>
+                      <div class="collapse-content">
+                        <li>
+                          <Link to="" class="">
+                            Profile
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="" class="">
+                            Dashboard
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="" onClick={logout}>
+                            Logout
+                          </Link>
+                        </li>
+                      </div>
+                    </div>
+                  </NavLink>
+                ) : (
+                  <NavLink to="/authentication/user">Login</NavLink>
+                )}
+              </li>
+            </>
+          </ul>
+        </div>
+        <div className="navbar-end hidden lg:flex min-w-fit">
+          <ul className="menu menu-horizontal  p-0">{menuItems}</ul>
         </div>
       </div>
     </div>
